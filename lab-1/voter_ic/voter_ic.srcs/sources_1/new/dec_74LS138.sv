af6548c4e121ef782d343534adc05b9fd24d313c
KuangjuX <18630816527@163.com>
2021-04-02 16:32:16 +0800

    child moudle

---
+++ lab-1/voter_ic/voter_ic.srcs/sources_1/new/dec_74LS138.sv
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/04/02 16:17:47
+// Design Name: 
+// Module Name: dec_74LS138
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dec_74LS138(
+       input [2:0] decode,
+       input [2:0] enable,
+       output logic [7:0] res
+    );
+    
+    always_comb begin
+       if (enable == 3'b100) begin
+        case (decode)
+            3'b000: res = 11111110;
+            3'b001: res = 11111101;
+            3'b010: res = 11111011;
+            3'b011: res = 11110111;
+            3'b100: res = 11101111;
+            3'b101: res = 11011111;
+            3'b110: res = 10111111;
+            3'b111: res = 01111111;
+        endcase
+         end
+       else res = 8'b11111111;
+    end
+    
+endmodule
---
+++ lab-1/voter_ic/voter_ic.srcs/sources_1/new/voter5.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/04/02 16:32:00
+// Design Name: 
+// Module Name: voter5
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module voter5(
+
+    );
+endmodule
