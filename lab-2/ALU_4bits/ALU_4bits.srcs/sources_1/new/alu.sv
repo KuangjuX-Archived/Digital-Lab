356aaf317cd3d236f97e60ee0c1e5b1b5a56c064
KuangjuX <18630816527@163.com>
2021-04-15 08:33:27 +0800

    lab-2 init

---
+++ lab-2/ALU_4bits/ALU_4bits.srcs/sources_1/new/alu.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/04/15 08:30:53
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input [3:0] A,
+    input [3:0] B,
+    input [3:0] aluop,
+    output logic [7:0] alures,
+    output logic ZF,
+    output logic OF
+    );
+endmodule
---
+++ lab-2/ALU_4bits/ALU_4bits.srcs/sources_1/new/rca.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/04/15 08:30:26
+// Design Name: 
+// Module Name: rca
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rca(
+
+    );
+endmodule
