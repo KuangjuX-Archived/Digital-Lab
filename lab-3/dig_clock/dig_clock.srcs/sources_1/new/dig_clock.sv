1f9f151d27535505a2913bacf6d9a5ef77de826b
KuangjuX <18630816527@163.com>
2021-05-12 13:08:25 +0800

    init lab-3

---
+++ lab-3/dig_clock/dig_clock.srcs/sources_1/new/dig_clock.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module dig_clock(
+    input                   sys_clk,
+    input                   sys_rst_n,
+    input                   i_start,
+    output logic [3 : 0]    an,
+    output logic [7 : 0]    a_to_g
+    );
+    
+ 
+    
+endmodule
