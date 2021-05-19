d80d656a8cf6de1f2e2a9bedf9862ce7d2850d42
KuangjuX <18630816527@163.com>
2021-05-19 10:34:29 +0800

    update dff_n

--- lab-3/dig_clock/dig_clock.srcs/sources_1/new/DFF_N.sv
+++ lab-3/dig_clock/dig_clock.srcs/sources_1/new/DFF_N.sv
@@ -29,10 +29,15 @@ module DFF_N (
 
     logic dff0_Q;
     logic dff1_Q;
-    always_ff@(posedge sys_clk)
-        if(i_start && sys_rst_n) dff0_Q <= 1;
-        else if(!i_start && sys_rst_n) dff0_Q <= 0;
-        else if(!sys_rst_n) dff0_Q <= 0;
-    assign start_flag = dff0_Q;
+    always_ff@(posedge sys_clk, posedge sys_rst_n)
+        if(sys_rst_n) begin 
+            dff0_Q <= i_start;
+            dff1_Q <= dff0_Q;
+        end
+        else if(!sys_rst_n) begin 
+            dff0_Q <= 0;
+            dff1_Q <= 1;
+        end
+    assign start_flag = (~dff1_Q)&dff0_Q;
 
 endmodule
