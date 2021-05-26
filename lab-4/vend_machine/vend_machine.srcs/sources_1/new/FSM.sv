6d28e652dc1667c66f10008eeccc5ae9a38f6bb7
KuangjuX <18630816527@163.com>
2021-05-26 16:57:14 +0800

    update

--- lab-4/vend_machine/vend_machine.srcs/sources_1/new/FSM.sv
+++ lab-4/vend_machine/vend_machine.srcs/sources_1/new/FSM.sv
@@ -46,62 +46,79 @@ module FSM #(parameter MAX_CNT = 250000000)(
         else current_state <= next_state;
     end
 
-    always@(*) begin
-        case(current_state)
+    always_ff@(posedge sys_clk) begin
+        if(!sys_clk) begin
+            price <= 8'd0;
+            change <= 8'd0;
+            open <= 1'b0;
+        end
+
+        else begin
+            case(current_state)
             4'd0: begin
-                if(coin5_flag) next_state = 4'd1;
-                else if(coin10_flag) next_state = 4'd2;
+                if(coin5_flag) next_state <= 4'd1;
+                else if(coin10_flag) next_state <= 4'd2;
             end
             4'd1: begin
-                if(coin5_flag) next_state = 4'd2;
-                else if(coin10_flag) next_state = 4'd3;
+                if(coin5_flag) next_state <= 4'd2;
+                else if(coin10_flag) next_state <= 4'd3;
             end
             4'd2: begin
-                if(coin5_flag) next_state = 4'd3;
-                else if(coin10_flag) next_state = 4'd4;
+                if(coin5_flag) next_state <= 4'd3;
+                else if(coin10_flag) next_state <= 4'd4;
             end
         endcase
+        end
     end
 
-    always@(*) begin
+    always_ff@(posedge sys_clk) begin
         if(!sys_rst_n) begin
             price = 8'd0;
             change = 8'd0;
-            open = 0;
+            open = 1'b0;
         end
 
         else begin
             case(next_state)
-                4'd0:begin
+                4'd0: begin
                     price = 8'd0;
                     change = 8'd0;
-                    open = 0;
+                    open = 1'b0;
                 end
 
-                4'd1:begin
+                4'd1: begin
                     price = 8'd5;
                     change = 8'd0;
-                    open = 0;
+                    open = 1'b0;
                 end
 
-                4'd2:begin
+                4'd2: begin
                     price = 8'd10;
                     change = 8'd0;
-                    open = 0;
+                    open = 1'b0;
                 end
 
-                4'd3:begin
+                4'd3: begin
                     price = 8'd15;
                     change = 8'b0;
-                    open = 1;
+                    open = 1'b1;
+                    sale_cnt Re1(
+                        .sys_clk(sys_clk),
+                        .sys_rst_n(sys_rst_n),
+                        .state(next_state)
+                    );
             
                 end
 
-                4'd4:begin 
+                4'd4: begin 
                     price = 8'd20;
                     change = 8'd5;
-                    open = 1;
-                    
+                    open = 1'b1;
+                    sale_cnt Re2(
+                        .sys_clk(sys_clk),
+                        .sys_rst_n(sys_rst_n),
+                        .state(next_state)
+                    );
                 end
             endcase   
         end
